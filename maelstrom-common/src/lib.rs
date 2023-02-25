c65733323814a623e83c782c2e2b41902e3dcdf6
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-02-25 02:04:40 -0600

    Use a better example in doctest
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- maelstrom-common/src/lib.rs
+++ maelstrom-common/src/lib.rs
@@ -14,7 +14,7 @@
 //! 
 //! ## Example
 //! 
-//! ```
+//! ```no_run
 //! use maelstrom_common::{Actor, Envelope};
 //! use serde_json::Value;
 //! 
@@ -24,6 +24,34 @@
 //!   node_id: Option<String>
 //! }
 //! 
+//! #[derive(Debug, Serialize, Deserialize, Clone)]
+//! #[serde(tag = "type")]
+//! pub enum Request {
+//!    #[serde(rename = "init")]
+//!    Init {
+//!        msg_id: usize,
+//!        node_id: String,
+//!    },
+//!    #[serde(rename = "echo")]
+//!    Echo {
+//!        echo: String,
+//!        msg_id: usize
+//!    }
+//!}
+//!
+//!#[derive(Debug, Serialize, Deserialize, Clone)]
+//!#[serde(tag = "type")]
+//!pub enum Response {
+//!    #[serde(rename = "init_ok")]
+//!    InitOk {
+//!        in_reply_to: usize,
+//!    },
+//!    #[serde(rename = "echo_ok")]
+//!    EchoOk {
+//!        echo: String,
+//!        in_reply_to: usize
+//!    }
+//!}
 //! 
 //! impl Echo {
 //!  pub fn new() -> Self {
@@ -31,25 +59,33 @@
 //!  }
 //! }
 //! 
-//! 
 //! impl Actor for Echo {
-//!    type InboundMessage = Value;
-//!    type OutboundMessage = Value;
+//!    type InboundMessage = Request;
+//!    type OutboundMessage = Response;
 //!    
 //!    fn handle_message(&mut self, msg: Envelope<Self::InboundMessage>) -> Option<Envelope<Self::OutboundMessage>> {
-//!       let mut payload = msg.body;
-//!       // do something with the payload.
-//!       // ...
-//!       // Return some message in a "return envelope".
-//!       Some(Envelope::new(msg.dest, msg.src, payload))
+//!
+//!        Some(match msg.body {
+//!            Request::Init { msg_id, node_id } => {
+//!                self.node_id = Some(node_id.clone());
+//!                eprintln!("[INIT] Initialized node: {}", node_id);
+//!                Envelope {
+//!                    body: Response::InitOk { in_reply_to: msg_id },
+//!                    src: msg.dest,
+//!                    dest: msg.src
+//!                }
+//!            },
+//!            Request::Echo { echo, msg_id } => {
+//!                eprintln!("[ECHO] Echoing back: {}, in reply to: {}", echo, msg_id);
+//!                Envelope {
+//!                    src: msg.dest,
+//!                    dest: msg.src,
+//!                    body: Response::EchoOk { echo, in_reply_to: msg_id }
+//!                }
+//!            },
+//!        })
 //!    }
 //! }
-//! 
-//! 
-//! pub fn main() {
-//!     maelstrom_common::run(Echo::new()).unwrap();
-//! }
-//! 
 //! ```
 //! 
 //! 
