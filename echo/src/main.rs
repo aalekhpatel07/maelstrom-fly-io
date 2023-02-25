43354b971de0feaab4f47214f6e6c838d49c7a1b
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-02-25 02:07:24 -0600

    Rustfmt fix.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,77 +1,64 @@
-use maelstrom_common::{
-    Actor, 
-    Envelope,
-    run
-};
-use serde::{Serialize, Deserialize};
-
+use maelstrom_common::{run, Actor, Envelope};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(tag = "type")]
 pub enum Request {
     #[serde(rename = "init")]
-    Init {
-        msg_id: usize,
-        node_id: String,
-    },
+    Init { msg_id: usize, node_id: String },
     #[serde(rename = "echo")]
-    Echo {
-        echo: String,
-        msg_id: usize
-    }
+    Echo { echo: String, msg_id: usize },
 }
-
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(tag = "type")]
 pub enum Response {
     #[serde(rename = "init_ok")]
-    InitOk {
-        in_reply_to: usize,
-    },
+    InitOk { in_reply_to: usize },
     #[serde(rename = "echo_ok")]
-    EchoOk {
-        echo: String,
-        in_reply_to: usize
-    }
+    EchoOk { echo: String, in_reply_to: usize },
 }
-
 
 #[derive(Debug, Default)]
 pub struct Echo {
     // Store our ID when a client initializes us.
-    node_id: Option<String>
+    node_id: Option<String>,
 }
-
 
 impl Actor for Echo {
     type InboundMessage = Request;
     type OutboundMessage = Response;
-    
-    fn handle_message(&mut self, msg: Envelope<Self::InboundMessage>) -> Option<Envelope<Self::OutboundMessage>> {
 
+    fn handle_message(
+        &mut self,
+        msg: Envelope<Self::InboundMessage>,
+    ) -> Option<Envelope<Self::OutboundMessage>> {
         Some(match msg.body {
             Request::Init { msg_id, node_id } => {
                 self.node_id = Some(node_id.clone());
                 eprintln!("[INIT] Initialized node: {}", node_id);
                 Envelope {
-                    body: Response::InitOk { in_reply_to: msg_id },
+                    body: Response::InitOk {
+                        in_reply_to: msg_id,
+                    },
                     src: msg.dest,
-                    dest: msg.src
+                    dest: msg.src,
                 }
-            },
+            }
             Request::Echo { echo, msg_id } => {
                 eprintln!("[ECHO] Echoing back: {}, in reply to: {}", echo, msg_id);
                 Envelope {
                     src: msg.dest,
                     dest: msg.src,
-                    body: Response::EchoOk { echo, in_reply_to: msg_id }
+                    body: Response::EchoOk {
+                        echo,
+                        in_reply_to: msg_id,
+                    },
                 }
-            },
+            }
         })
     }
 }
-
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     run(Echo::default())?;
--- unique-id-generation/src/main.rs
+++ unique-id-generation/src/main.rs
@@ -1,73 +1,62 @@
-use serde::{Serialize, Deserialize};
+use maelstrom_common::{formatted_log, run, Actor, Envelope};
+use serde::{Deserialize, Serialize};
 use std::sync::atomic::AtomicUsize;
-use maelstrom_common::{
-    Actor,
-    Envelope,
-    formatted_log,
-    run
-};
-
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(tag = "type")]
 pub enum Request {
     #[serde(rename = "generate")]
-    Generate {
-        msg_id: usize,
-    },
+    Generate { msg_id: usize },
     #[serde(rename = "init")]
     Init {
         msg_id: usize,
         node_id: String,
         node_ids: Vec<String>,
-    }
+    },
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(tag = "type")]
 pub enum Response {
     #[serde(rename = "generate_ok")]
-    GenerateOk {
-        id: String,
-        in_reply_to: usize,
-    },
+    GenerateOk { id: String, in_reply_to: usize },
     #[serde(rename = "init_ok")]
-    InitOk {
-        in_reply_to: usize,
-    },
+    InitOk { in_reply_to: usize },
 }
-
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 pub struct UniqueUuid {
     pub counter: AtomicUsize,
-    pub node_id: Option<String>
+    pub node_id: Option<String>,
 }
-
 
 impl Actor for UniqueUuid {
     type InboundMessage = Request;
     type OutboundMessage = Response;
     fn handle_message(
         &mut self,
-        msg: Envelope<Self::InboundMessage>
+        msg: Envelope<Self::InboundMessage>,
     ) -> Option<Envelope<Self::OutboundMessage>> {
         Some(match msg.body {
-            Request::Init { msg_id, node_id, .. } => {
+            Request::Init {
+                msg_id, node_id, ..
+            } => {
                 self.node_id = Some(node_id.clone());
                 formatted_log!("INIT", "Initialized node with id: {}", node_id);
 
                 Envelope {
                     body: Response::InitOk {
-                        in_reply_to: msg_id
+                        in_reply_to: msg_id,
                     },
                     src: msg.dest,
-                    dest: msg.src
+                    dest: msg.src,
                 }
-            },
+            }
             Request::Generate { msg_id } => {
-                let counter = self.counter.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-                
+                let counter = self
+                    .counter
+                    .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+
                 // Just namespace the ids with the node id and Bob's your uncle.
                 let id = format!("{}-{}", self.node_id.as_ref().unwrap(), counter);
 
@@ -76,16 +65,15 @@ impl Actor for UniqueUuid {
                 Envelope {
                     body: Response::GenerateOk {
                         id,
-                        in_reply_to: msg_id
+                        in_reply_to: msg_id,
                     },
                     src: msg.dest,
-                    dest: msg.src
+                    dest: msg.src,
                 }
             }
         })
     }
 }
-
 
 pub fn main() {
     run(UniqueUuid::default()).unwrap();
