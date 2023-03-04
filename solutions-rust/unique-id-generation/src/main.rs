d80321a3fd3dedface8efc5e76d223769cc9558d
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-03-03 19:43:35 -0600

    Refactor solution for unique id generation challenge
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- solutions-rust/unique-id-generation/src/main.rs
+++ solutions-rust/unique-id-generation/src/main.rs
@@ -1,27 +1,37 @@
-use maelstrom_common::{formatted_log, run, Actor, Envelope, Message};
+use maelstrom_common::{run, Actor, Envelope};
 use serde::{Deserialize, Serialize};
 use std::sync::atomic::AtomicUsize;
+use thiserror::Error;
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(tag = "type")]
-pub enum Request {
-    #[serde(rename = "generate")]
-    Generate { msg_id: usize },
-    #[serde(rename = "init")]
-    Init {
-        msg_id: usize,
-        node_id: String,
-        node_ids: Vec<String>,
-    },
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("The generate challenge does not expect to receive any response kind of message but received: {0}")]
+    ReceivedUnexpectedResponseMessage(String),
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+
+#[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type")]
-pub enum Response {
+pub enum Message {
+    #[serde(rename = "generate")]
+    Generate { msg_id: Option<usize> },
+    #[serde(rename = "init")]
+    Init {
+        #[serde(skip_serializing_if = "Option::is_none")]
+        msg_id: Option<usize>,
+        node_id: String,
+    },
     #[serde(rename = "generate_ok")]
-    GenerateOk { id: String, in_reply_to: usize },
+    GenerateOk { 
+        id: String, 
+        #[serde(skip_serializing_if = "Option::is_none")]
+        in_reply_to: Option<usize>
+    },
     #[serde(rename = "init_ok")]
-    InitOk { in_reply_to: usize },
+    InitOk {
+        #[serde(skip_serializing_if = "Option::is_none")]
+        in_reply_to: Option<usize>,
+    },
 }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
@@ -31,42 +41,40 @@ pub struct UniqueUuid {
 }
 
 impl Actor for UniqueUuid {
-    type InboundMessage = Request;
-    type OutboundMessage = Response;
+    type Message = Message;
+    type Error = Error;
+
     fn handle_message(
         &mut self,
-        msg: Envelope<Message<Self::InboundMessage, Self::OutboundMessage>>,
-    ) -> Option<Envelope<Message<Self::InboundMessage, Self::OutboundMessage>>> {
-        Some(match msg.body {
-            Message::Inbound(Request::Init {
+        msg: Envelope<Self::Message>,
+        outbound_msg_tx: std::sync::mpsc::Sender<Envelope<Self::Message>>,
+    ) -> Result<(), Self::Error> {
+        match msg.body {
+            Message::Init {
                 msg_id, ref node_id, ..
-            }) => {
+            } => {
                 self.node_id = Some(node_id.clone());
-                formatted_log!("INIT", "Initialized node with id: {}", node_id);
-
-                msg.reply(Message::Outbound(Response::InitOk { in_reply_to: msg_id }))
+                let reply = msg.reply(Message::InitOk { in_reply_to: msg_id });
+                outbound_msg_tx.send(reply).unwrap();
+                Ok(())
             }
-            Message::Inbound(Request::Generate { msg_id }) => {
+            Message::Generate { msg_id } => {
                 let counter = self
                     .counter
                     .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
 
                 // Just namespace the ids with the node id and Bob's your uncle.
                 let id = format!("{}-{}", self.node_id.as_ref().unwrap(), counter);
+                let reply = msg.reply(Message::GenerateOk {
+                    id,
+                    in_reply_to: msg_id,
+                });
+                outbound_msg_tx.send(reply).unwrap();
 
-                formatted_log!("GENERATE", "Generated id: {}", id);
-                
-                msg.reply(
-                    Message::Outbound(Response::GenerateOk {
-                        id,
-                        in_reply_to: msg_id,
-                    })
-                )
+                Ok(())
             },
-            _ => {
-                panic!("Unexpected message: {:?}", msg)
-            }
-        })
+            _ => Err(Error::ReceivedUnexpectedResponseMessage(serde_json::to_string_pretty(&msg).unwrap()))
+        }
     }
 }
 
