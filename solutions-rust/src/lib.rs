a179d756997515cf1a0c24a520de0f1bceec0048
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-03-12 23:02:30 -0500

    Add solution for problem 4.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- solutions-rust/src/lib.rs
+++ solutions-rust/src/lib.rs
@@ -6,9 +6,6 @@ use std::sync::mpsc::Sender;
 mod envelope;
 pub use envelope::*;
 
-mod pubsub;
-pub use pubsub::*;
-
 pub fn read_stdin<B: Debug + DeserializeOwned>(incoming_messages_tx: Sender<Envelope<B>>) {
     for line in io::stdin().lines().map(Result::unwrap) {
         let decoded = serde_json::from_str(&line).unwrap();
